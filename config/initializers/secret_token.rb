97ab6337b3678375f2ebe54da6bd0cf0146367d2
Geng Wang <gengwang04@gmail.com>
2014-02-23 21:00:47 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sky3::Application.config.secret_key_base = '7408352c785d2f35535e0c17d2537e621c5cc337d236759bd1862b0e4b04a7694d5e7fb201cf65cd28b34f54245192ab23b2d72caa3e7a687ccb0499f80fa819'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sky3::Application.config.session_store :cookie_store, key: '_sky3_session'
